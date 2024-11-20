945c4873a5f36f6770643f069b5163931f9a1dbe
Mazzomes_Clone <lukemadisonbarker@gmail.com>
2024-11-20 19:43:50 +1300

    Revert vercel settings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const app = express()
 
 require('dotenv').config()
-// require('./dbConnect')
+require('./dbConnect')
 
 const PORT = process.env.PORT
 const appName = "TurnUp"
